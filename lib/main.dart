8cc09095c782130f1ce18d5fa04788a07daa9af5
Ahmed Mahmoud <88392155+ahmedmahmoud72@users.noreply.github.com>
2023-09-15 23:33:41 +0300

    Implement Lesson Details API

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'layout/app_cubit/app_cubit.dart';
-import 'layout/home.dart';
+import 'layout/layout.dart';
 import 'modules/onboarding/onboarding_screen.dart';
 
 Future<void> main() async {
--- lib/modules/auth/login/login_screen.dart
+++ lib/modules/auth/login/login_screen.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../../../layout/home.dart';
+import '../../../layout/layout.dart';
 import '../../../shared/components/custom_widgets/custom_text_form_field.dart';
 import '../../../shared/components/custom_widgets/custom_toast.dart';
 import '../../../shared/components/custom_widgets/main_button.dart';
