91be538e77932054dc9499b8d39057bd08a4ece1
Ahmed Mahmoud <88392155+ahmedmahmoud72@users.noreply.github.com>
2023-09-19 23:10:02 +0300

    Handle SignOut Logic

--- lib/shared/components/constants/navigation_helper.dart
+++ lib/shared/components/constants/navigation_helper.dart
@@ -1,15 +1,29 @@
+import 'package:e_learning/modules/auth/login/login_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../../network/local/shared_preferences_helper.dart';
+
 void navigateTo(context, widget) => Navigator.push(
-  context,
-  MaterialPageRoute(
-    builder: (context) => widget,
-  ),
-);
+      context,
+      MaterialPageRoute(
+        builder: (context) => widget,
+      ),
+    );
 
 void navigateToAndKill(context, widget) => Navigator.pushAndRemoveUntil(
     context,
     MaterialPageRoute(
       builder: (context) => widget,
     ),
-        (Route<dynamic> route) => false);
+    (Route<dynamic> route) => false);
+
+void singOut(context) {
+  CacheHelper.removeData(
+    key: 'token',
+  )!
+      .then((value) {
+    if (value) {
+      navigateToAndKill(context, LoginScreen());
+    }
+  });
+}
