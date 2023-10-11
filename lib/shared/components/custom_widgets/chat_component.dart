ee045a64b9f6ad63cd14c1600e838489cc9867e7
Ahmed Mahmoud <88392155+ahmedmahmoud72@users.noreply.github.com>
2023-10-11 16:02:49 +0300

    Implement comments component and Update UI

--- lib/shared/components/custom_widgets/chat_component.dart
+++ lib/shared/components/custom_widgets/chat_component.dart
@@ -9,62 +9,65 @@ class ChatComponent extends StatelessWidget {
   String date;
 
   ChatComponent({
-  required this.title,
-  required this.subTitle,
-  required this.image,
-  this.function,
-  required this.date,
-  super.key,
-});
+    required this.title,
+    required this.subTitle,
+    required this.image,
+    this.function,
+    required this.date,
+    super.key,
+  });
 
-@override
-Widget build(BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.only(left: 20,right: 20,bottom: 5.0,top: 5.0),
-    child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Colors.white,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: ListTile(
-            onTap: (){
-              function;
-            },
-            leading: Stack(
-                children:[ CircleAvatar(
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          const EdgeInsets.only(left: 20, right: 20, bottom: 5.0, top: 5.0),
+      child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ListTile(
+              onTap: () {
+                function;
+              },
+              leading: Stack(children: [
+                CircleAvatar(
                   radius: 25.0,
                   backgroundImage: AssetImage(image),
                 ),
-                  Positioned(
-                    right: 0,
-                    bottom: 0,
-                    child: Container(
-                      width: 10.0,
-                      height: 10.0,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.green,
-                      ),
+                Positioned(
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    width: 10.0,
+                    height: 10.0,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.green,
                     ),
                   ),
-                ]),
-            title: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: Text(
-                title,
-                style: GoogleFonts.poppins(fontWeight: FontWeight.w600, fontSize: 16.0),
+                ),
+              ]),
+              title: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  title,
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600, fontSize: 16.0),
+                ),
+              ),
+              subtitle: Text(
+                subTitle,
+              ),
+              trailing: Text(
+                date,
+                style: GoogleFonts.notoSans(),
               ),
             ),
-            subtitle: Text(
-              subTitle,
-            ),
-            trailing: Text(date, style: GoogleFonts.notoSans(),),
-
-          ),
-        )
-    ),
-  );
-}
+          )),
+    );
+  }
 }
