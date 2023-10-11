c8126f5b5fc44f5a35474370e3145c0ecd2ba095
Ahmed Mahmoud <88392155+ahmedmahmoud72@users.noreply.github.com>
2023-10-11 16:06:08 +0300

    Update Settings UI

--- lib/modules/chat/online_users_screen.dart
+++ lib/modules/chat/online_users_screen.dart
@@ -11,29 +11,33 @@ class ChatScreen extends StatelessWidget {
       child: Scaffold(
         body: SingleChildScrollView(
           child:
-          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 80.0),
+                child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text(
-                'Messages',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyLarge!
-                    .copyWith(fontSize: 24,fontWeight: FontWeight.bold),
-              ),
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'Messages',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
             ),
             ListView(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                children: dummyOnlineUsers
-                    .map((e) => ChatComponent(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  children: dummyOnlineUsers
+                      .map(
+                        (e) => ChatComponent(
                           title: e.title,
                           subTitle: e.subTitle,
                           image: e.image,
                           date: e.date,
-                        ))
-                    .toList()),
+                        ),
+                      ).toList()),
           ]),
+              ),
         ),
       ),
     );
